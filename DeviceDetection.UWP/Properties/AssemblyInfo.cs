4ab20aab71995d311a05e7560ef22d4fb4f8ba2a
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2017-05-27 09:29:53 +0430

    Add nuget and change pack description

--- DeviceDetection.UWP/Properties/AssemblyInfo.cs
+++ DeviceDetection.UWP/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DeviceDetection.UWP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Detect what is device type")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Hussein.Juybari")]
 [assembly: AssemblyProduct("DeviceDetection.UWP")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
